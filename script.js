5cfb7a14a72f2675293391a703030321db326105
uizsko <131705387+uizsko@users.noreply.github.com>
2024-02-28 19:42:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,71 +1,199 @@
-let currentCurrency = 0;
 
-let targetCurrency = 0;
+//first book - 1
+document.getElementById("renamefr").addEventListener("click", function () {
+    document.getElementById("name1").style.display = "none";
+    document.getElementById("inpr1").style.display = "block";
+    document.getElementById("ap1").style.display = "block";
+})
+document.getElementById("ap1").addEventListener("click", function () {
+    let newName = document.getElementById("inpr1").value;
+    document.getElementById("name1").innerHTML = newName;
 
-
-
-
-document.getElementById("rad1").addEventListener("change", function () {
-    currentCurrency = 1;
-});
-
-document.getElementById("rad2").addEventListener("change", function () {
-    currentCurrency = 2;
-});
-
-document.getElementById("rad3").addEventListener("change", function () {
-    currentCurrency = 3;
-});
-
-document.getElementById("rad4").addEventListener("change", function () {
-
-    targetCurrency = 4;
-});
-
-document.getElementById("rad5").addEventListener("change", function () {
-    targetCurrency = 5;
-});
-
-document.getElementById("rad6").addEventListener("change", function () {
-    targetCurrency = 6;
-});
+    document.getElementById("inpr1").style.display = "none";
+    document.getElementById("ap1").style.display = "none";
+    document.getElementById("name1").style.display = "block";
+})
 
 
+//second book - 2
+document.getElementById("renamefr2").addEventListener("click", function () {
+    document.getElementById("name2").style.display = "none";
+    document.getElementById("inpr2").style.display = "block";
+    document.getElementById("ap2").style.display = "block";
+})
+document.getElementById("ap2").addEventListener("click", function () {
+    let newName = document.getElementById("inpr2").value;
+    document.getElementById("name2").innerHTML = newName;
+
+    document.getElementById("inpr2").style.display = "none";
+    document.getElementById("ap2").style.display = "none";
+    document.getElementById("name2").style.display = "block";
+})
 
 
+//third book - 3
+document.getElementById("renamefr3").addEventListener("click", function () {
+    document.getElementById("name3").style.display = "none";
+    document.getElementById("inpr3").style.display = "block";
+    document.getElementById("ap3").style.display = "block";
+})
+document.getElementById("ap3").addEventListener("click", function () {
+    let newName = document.getElementById("inpr3").value;
+    document.getElementById("name3").innerHTML = newName;
 
-let result = 0;
+    document.getElementById("inpr3").style.display = "none";
+    document.getElementById("ap3").style.display = "none";
+    document.getElementById("name3").style.display = "block";
+})
 
-function getValue() {
-    let value = document.getElementById("userValue").value;
-    //usd to usd
-    if (currentCurrency == 1 && targetCurrency == 4) {
-        document.getElementById("result").innerHTML = value;
-        //usd to eur
-    } else if (currentCurrency == 1 && targetCurrency == 5) {
-        document.getElementById('result').innerHTML = value * 0.92;
-        //usd to uah                        
-    } else if (currentCurrency == 1 && targetCurrency == 6) {
-        document.getElementById('result').innerHTML = value * 38.48;
-        //eur to usd
-    } else if (currentCurrency == 2 && targetCurrency == 4) {
-        document.getElementById('result').innerHTML = value * 1.08;
-        //eur to eur
-    } else if (currentCurrency == 2 && targetCurrency == 5) {
-        document.getElementById('result').innerHTML = value;
-        //eur to uah
-    } else if (currentCurrency == 2 && targetCurrency == 6) {
-        document.getElementById('result').innerHTML = value * 41.60;
-        //uah to usd
-    } else if (currentCurrency == 3 && targetCurrency == 4) {
-        document.getElementById('result').innerHTML = value * 0.026;
-        //uah to eur
-    } else if (currentCurrency == 3 && targetCurrency == 5) {
-        document.getElementById('result').innerHTML = value * 0.024;
-        //uah to uah
-    } else if (currentCurrency == 3 && targetCurrency == 6) {
-        document.getElementById('result').innerHTML = value;
+//fourth book - 4
+document.getElementById("renamefr4").addEventListener("click", function () {
+    document.getElementById("name4").style.display = "none";
+    document.getElementById("inpr4").style.display = "block";
+    document.getElementById("ap4").style.display = "block";
+})
+document.getElementById("ap4").addEventListener("click", function () {
+    let newName = document.getElementById("inpr4").value;
+    document.getElementById("name4").innerHTML = newName;
+
+    document.getElementById("inpr4").style.display = "none";
+    document.getElementById("ap4").style.display = "none";
+    document.getElementById("name4").style.display = "block";
+})
+
+//fifth book - 5
+document.getElementById("renamefr5").addEventListener("click", function () {
+    document.getElementById("name5").style.display = "none";
+    document.getElementById("inpr5").style.display = "block";
+    document.getElementById("ap5").style.display = "block";
+})
+document.getElementById("ap5").addEventListener("click", function () {
+    let newName = document.getElementById("inpr5").value;
+    document.getElementById("name5").innerHTML = newName;
+
+    document.getElementById("inpr5").style.display = "none";
+    document.getElementById("ap5").style.display = "none";
+    document.getElementById("name5").style.display = "block";
+})
+
+
+document.getElementById("addBook").addEventListener("click", function () {
+    alert("works");
+    let div = document.createElement("div");
+    div.className = "wrap";
+    div.id = "new1";
+    document.getElementById("bookList").appendChild(div);
+
+    let input = document.createElement("input");
+    input.type = "text";
+    input.className = "InpRename";
+    input.id = "inprn1";
+    input.style.display = "block"
+    document.getElementById("new1").appendChild(input);
+
+    let buttonA = document.createElement("button");
+    buttonA.className = "apply";
+    buttonA.id = "applyn1";
+    buttonA.innerHTML = "Apply";
+    buttonA.style.display = "block";
+    document.getElementById("new1").appendChild(buttonA);
+
+    let buttonR = document.createElement("button");
+    buttonR.className = "rename";
+    buttonR.id = "renamefrn1";
+    buttonR.innerHTML = "Rename";
+    document.getElementById("new1").appendChild(buttonR);
+
+    let buttonD = document.createElement("button");
+    buttonD.className = "delete";
+    buttonD.id = "deleten1";
+    buttonD.innerHTML = "Delete";
+    document.getElementById("new1").appendChild(buttonD);
+})
+
+document.getElementById("addBook").addEventListener("click", function () {
+    for (let i = 0; i++) {
+        alert("works");
+        let div i = document.createElement("div")
+
     }
+})
 
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function GetDelete() {
+//     alert("works")
+//     document.getElementsByClassName("firstBook").remove();
+
+// }
+
+
+// document.getElementById("delete").addEventListener("click", function getDelete() {
+//     alert("works")
+//     for (i = 1, i <= )
+//         document.getElementById("1").remove();
+// })
+
+
+// document.querySelector('#books').oninput = function () {
+//     let val = this.value.trim();
+//     let elasticItems = document.querySelectorAll('.elastic li');
+//     if (val != "") {
+//         elasticItems.forEach(function (elem) {
+//             if (elem.innerText.search(val) == -1) {
+//                 elem.classList.add('hide');
+//             }
+//             else {
+//                 elem.classList.remove("hide");
+
+//             }
+
+//         });
+//     }
+// }
+
+
+
+
